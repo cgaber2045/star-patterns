668bf786f419f8f006eae7894ee04be199f98799
cgaber2045 <cgaber2045@greenvilleschools.us>
2018-01-22 10:21:01 -0500

    Initial

--- StarPatterns.java
+++ StarPatterns.java
@@ -12,67 +12,231 @@
 
 class StarPatterns 
 {
-	public static void starGrid(int h, int w)
-	{			
-		for (int i=0; i<h; i++)
-		{
-			for (int j=0; j<w; j++)
-			{				
-				System.out.print(i + "" + j + " ");
-			}
-			System.out.println();
-		}	
-	}
-	
-	public static void starBox(int h, int w)
-	{
-		for (int i=0; i<h; i++)
-		{
-			for (int j=0; j<w; j++)
-			{	
-			    if (i==0 || i==h-1 || j==0 || j==w-1) System.out.print("*");
-			    else System.out.print(" ");
-				//System.out.print(i + "" + j + " ");
-			}
-			System.out.println();
-		}	
-	}
+    public static void starGrid(int h, int w)
+    {           
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {               
+                System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+    
+    public static void starBox(int h, int w)
+    {
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {   
+                if (i==0 || i==h-1 || j==0 || j==w-1) System.out.print("*");
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
 
-	public static void starX(int h)
-	{
-		int w = h;
+    public static void starX(int h)
+    {
+        int w = h;
 
-		for (int i=0; i<h; i++)
-		{
-			for (int j=0; j<w; j++)
-			{
-			    if (i==j || i+j==h-1) System.out.print("*");
-			    else System.out.print(" ");
-			    System.out.print(i + "" + j + " ");
-			}
-			System.out.println();
-		}	
-	}
-	
-	public static void starZ(int h)
-	{
-	
-	}
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (i==j || i+j==h-1) System.out.print("*");
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+    
+    public static void starZ(int h)
+    {
+        int w = h;
 
-	public static void main(String[] args) 
-	{
-		starGrid(5, 5);
-		System.out.println();
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (i == 0 || i == h-1 || i+j==h-1) System.out.print("*");
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+    
+    public static void starXBox(int h)
+    {
+        int w = h;
 
-		starBox(7, 9);
-		System.out.println();
-		
-		starX(7);
-		System.out.println();
-		
-		starZ(7);
-		System.out.println();
-			
-	}
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (i==j || i+j==h-1 || i == 0 || i == h-1 || j == 0 || j == w-1) System.out.print("*");
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+
+    public static void twoStarBoxes(int h, int w)
+    {
+
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if ((i < h/2 && j < w/2) || (i >= h/2 && j >= w/2)) System.out.print("*");
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+
+    public static void starTriangle(int h)
+    {
+        int w = h;
+        
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (i >= j) System.out.print("*");
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+
+    public static void emptyTriangle(int h)
+    {
+        int w = h;
+        
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (i >= j) System.out.print("*");
+                else System.out.print(" ");
+                System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+
+    public static void starTriangleUR(int h)
+    {
+        int w = h;
+        
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (i <= j) System.out.print("*");
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+    
+    public static void isoscelesStarTriangle(int h)
+    {
+        int w = h*2 - 1;
+        int count = 0;
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (j >= (w / 2) - count && j <= (w / 2) + count){
+                    System.out.print("*");
+                }
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            count += 1;
+            System.out.println();
+        }   
+    }
+    
+    public static void checkerBoard(int w, int h)
+    {
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if ((i % 2 != 0 && j % 2 != 0) || (i % 2 == 0 && j % 2 == 0)){
+                    System.out.print("*");
+                }
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+
+    public static void biggerCheckerBoard(int w, int h)
+    {
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if ((i % 2 != 0 && j % 2 != 0) || (i % 2 == 0 && j % 2 == 0)){
+                    System.out.print("*");
+                }
+                else System.out.print(" ");
+                //System.out.print(i + "" + j + " ");
+            }
+            System.out.println();
+        }   
+    }
+    
+    public static void main(String[] args) 
+    {
+        starGrid(5, 5);
+        System.out.println();
+
+        starBox(7, 9);
+        System.out.println();
+        
+        starX(7);
+        System.out.println();
+        
+        starZ(7);
+        System.out.println();
+        
+        starXBox(9);
+        System.out.println();
+        
+        twoStarBoxes(8, 12);
+        System.out.println();
+        
+        starTriangle(6);
+        System.out.println();
+        
+        emptyTriangle(6);
+        System.out.println();
+        
+        starTriangleUR(10);
+        System.out.println();
+        
+        isoscelesStarTriangle(5);
+        System.out.println();
+        
+        checkerBoard(8, 6);
+        System.out.println();
+        
+        biggerCheckerBoard(8, 6);
+        System.out.println();
+    }
      
 }
